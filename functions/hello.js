562f492963a7cb080fcf7549eb2e8ef48dd991a2
Victor Ferreira <victor.araujo.ferreira@gmail.com>
2019-04-10 13:42:05 -0300

    fix function

--- functions/hello.js
+++ functions/hello.js
@@ -2,6 +2,6 @@ exports.handler = function(event, context, callback) {
   const rndm = Math.floor(Math.random() * 1000 + 1);
   callback(null, {
     statusCode: 200,
-    body: rndm
+    body: `${rndm}`
   });
 };
