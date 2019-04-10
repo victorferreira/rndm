aa37f429fa2097c9c4efe1d14d9ccca02489ca14
Victor Ferreira <victor.araujo.ferreira@gmail.com>
2019-04-10 13:38:08 -0300

    fix lambda build

--- functions/hello.js
+++ functions/hello.js
@@ -1,6 +1,7 @@
 exports.handler = function(event, context, callback) {
+  const rndm = Math.floor(Math.random() * 1000 + 1);
   callback(null, {
     statusCode: 200,
-    body: "Hello, World"
+    body: rndm
   });
 };
